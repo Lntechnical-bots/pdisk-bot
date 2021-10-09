cdcf766a75d095327068e7f13c6c6c7a4c3afe36
lokaman chendekar <82169706+lntechnical2@users.noreply.github.com>
2021-10-09 08:49:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask , render_template , request
 from werkzeug.utils import secure_filename
 from flask import send_file
-import os 
+import os, os.path
 app = Flask(__name__)
 
 		
 @app.route('/<file_name>')
 def upload_fil(file_name):
    try:
+	DIR = '/storage/emulated/0/Download'
+	print(len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
    	path = f"/app/downloads/{file_name}"
    	return send_file(path, as_attachment=True)
    except:
